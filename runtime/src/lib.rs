fb20033e5a0e71dca4856fc3f9a6ceda1c37dd84
Benoit Devos <benoit@logion.network>
2024-04-03 15:01:46 +0200

    feat: apply semantic versioning for spec_version.
    logion-network/logion-internal#1199

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -186,7 +186,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("logion"),
 	impl_name: create_runtime_str!("logion"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 000_003_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
